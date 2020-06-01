8efa061df4bd654411f5b8cbc585d8c1b0b8bce8
PMenart <65567492+PMenart@users.noreply.github.com>
2020-06-01 15:28:50 -0400

    Update Devices.py

--- Python/Devices.py
+++ Python/Devices.py
@@ -11,27 +11,27 @@ x = {} #key by (x,y) w/ type of logic at that location
 NONEs=[(i,j) for i,j in np.ndindex(X,Y) if i==0 or j==0]
 
 # The 5CSEBA6U23I7 has occasional vertical strips for memory and DSP.
-RAM_Ys=[5,14,20,26,32,38,41,44,49,54,58,69,76,86,89]
-RAMs=[(i,j) for i,j in np.ndindex(X,Y) if j in RAM_Ys]
+RAM_Xs=[5,14,20,26,32,38,41,44,49,54,58,69,76,86,89]
+RAMs=[(i,j) for i,j in np.ndindex(X,Y) if i in RAM_Xs]
 
 # The 5CSEBA6U23I7 also has strips of MLABCELLs.
-MLAB_Ys=[3,6,8,15,21,25,28,34,39,47,52,59,65,72,78,82,84,87]
-MLABs=[(i,j) for i,j in np.ndindex(X,Y) if j in MLAB_Ys]
+MLAB_Xs=[3,6,8,15,21,25,28,34,39,47,52,59,65,72,78,82,84,87]
+MLABs=[(i,j) for i,j in np.ndindex(X,Y) if i in MLAB_Xs]
 
 # The 5CSEBA6U23I7 has two blank sections on the left of the chip.
 NONE_Xs=[i for i in range(1,10)]
 NONE_Ys=[i for i in range(15,32)]+[i for i in range(56,73)]
-NONEs.append([(i,j) for i,j in np.ndindex(X,Y) if i in NONE_Xs and j in NONE_Ys])
+NONEs.extend([(i,j) for i,j in np.ndindex(X,Y) if i in NONE_Xs and j in NONE_Ys])
 
 # The 5CSEBA6U23I7 has a HPS on the right side of the chip.
 NONE_Xs=[i for i in range(51,89)]
 NONE_Ys=[i for i in range(37,81)]
-NONEs.append([(i,j) for i,j in np.ndindex(X,Y) if i in NONE_Xs and j in NONE_Ys])
+NONEs.extend([(i,j) for i,j in np.ndindex(X,Y) if i in NONE_Xs and j in NONE_Ys])
 
 # The 5CSEBA6U23I7 has a horizontal strip connecting to the HPS.
 NONE_Xs=[i for i in range(45,51)]
-NONE_Xs=[i for i in range(37,38)]
-NONEs.append([(i,j) for i,j in np.ndindex(X,Y) if i in NONE_Xs and j in NONE_Ys])
+NONE_Ys=[i for i in range(37,38)]
+NONEs.extend([(i,j) for i,j in np.ndindex(X,Y) if i in NONE_Xs and j in NONE_Ys])
 
 for i,j in np.ndindex(X,Y): #thread over given values
     v=(i,j)
@@ -45,10 +45,10 @@ for i,j in np.ndindex(X,Y): #thread over given values
             else:
                 x[v][l]["TYPE"]="FF"
 
-    if v in RAMs:
+    elif v in RAMs:
         x[v].update({"TYPE":"RAM"})
 
-    if v in NONEs:
+    elif v in NONEs:
         x[v].update({"TYPE":"NONE"})
 
     else: #LABs
