a67b72f619c3b52edf0cb198d1cd929edb08de31
Noeloikeau <noeloikeau@gmail.com>
2020-07-22 14:23:17 -1000

    Update CarryChains.v

--- Modules/CarryChains.v
+++ Modules/CarryChains.v
@@ -4,9 +4,12 @@ module CarryChain(
   b,
   cin,
   sout,
-  clk
-
+  clk,
+  cout,
+  clr,
+  ena
   );
+ input clr,ena;
 //size of chain
   parameter N=16;
  //carry in
@@ -18,6 +21,7 @@ module CarryChain(
   input clk;
 //data out from registers
   output [N-1:0] sout;
+  output cout;
 //internal wire for passing carries
 //one bit larger for initial combinatorial cell
 //which passes data_in along carry chain
@@ -45,12 +49,14 @@ module CarryChain(
     primitive_ff myff(
     	.s(s[i-1]),
     	.clk(clk),
-    	.q(sout[i-1])
+    	.q(sout[i-1]),
+      .clr(clr),
+      .ena(ena)
     );
 
   end
   endgenerate
-
+  assign cout=c_internal[N];
   endmodule
 
   module primitive_carry_in(
@@ -135,10 +141,12 @@ endmodule
 module primitive_ff(
 	s,
 	clk,
-	q
+	q,
+  clr,
+  ena
 );
 
-input s,clk;
+input s,clk,clr,ena;
 output q;
 
 cyclonev_ff myff (
@@ -146,97 +154,13 @@ cyclonev_ff myff (
     .clk(clk),
     .clrn(),
     .aload(),
-    .sclr(),
+    .sclr(clr),
     .sload(),
     .asdata(),
-    .ena(),
+    .ena(ena),
     .devclrn(),
     .devpor(),
     .q(q)
 );
-
-endmodule
-
-module CARRY_CHAIN_LPM(
-  a,b,cin,cout,s,clock,clken,aclr
-  );
-
-  parameter width = 3;
-  parameter pipeline = 0;
-  //next are adder specific to force carry chain
-  localparam lpm_type = "lpm_add_sub";
-  localparam lpm_width = width;
-  localparam lpm_direction = "ADD";
-  localparam lpm_representation = "UNSIGNED";
-  localparam lpm_pipeline = pipeline;
-  localparam lpm_hint = "ONE_INPUT_IS_CONSTANT=YES,CIN_USED=YES,MAXIMIZE_SPEED=10";
-
-  input [lpm_width-1:0] a, b;
-  input cin;
-  output cout;
-  output [lpm_width-1:0] s;
-  input clock;
-  input clken;
-  input aclr;
-
-  lpm_add_sub #(
-    .lpm_type(lpm_type),
-    .lpm_width(lpm_width),
-    .lpm_direction(lpm_direction),
-    .lpm_representation(lpm_representation),
-    .lpm_pipeline(lpm_pipeline),
-    .lpm_hint(lpm_hint)
-
-    ) ADD (
-
-    .dataa(a),
-    .datab(b),
-    .cin(cin),
-    .clock(clock),
-    .clken(clken),
-    .aclr(aclr),
-    .result(s),
-    .cout(cout)
-    //.overflow(0)
-    //.add_sub(0)
-    );
-
-endmodule
-
-
-module CARRY_ONE(
-  a,b,cin,cout,s
-  );
-
-  parameter width = 3;
-  //next are adder specific to force carry chain
-  localparam lpm_type = "lpm_add_sub";
-  localparam lpm_width = width;
-  localparam lpm_direction = "ADD";
-  localparam lpm_representation = "UNSIGNED";
-  localparam lpm_pipeline = 0;
-  localparam lpm_hint = "ONE_INPUT_IS_CONSTANT=YES,CIN_USED=YES,MAXIMIZE_SPEED=10";
-
-  input [lpm_width-1:0] a, b;
-  input cin;
-  output cout;
-  output [lpm_width-1:0] s;
-
-  lpm_add_sub #(
-    .lpm_type(lpm_type),
-    .lpm_width(lpm_width),
-    .lpm_direction(lpm_direction),
-    .lpm_representation(lpm_representation),
-    .lpm_pipeline(lpm_pipeline),
-    .lpm_hint(lpm_hint)
-
-    ) ADD (
-
-    .dataa(a),
-    .datab(b),
-    .cin(cin),
-    .result(s),
-    .cout(cout)
-    );
 
 endmodule
